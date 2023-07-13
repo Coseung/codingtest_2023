850d9d5753faa0ba2bef82f2d3d1a9516010ad28
최승호 <dreamdp01@sungkyul.ac.kr>
2023-07-13 06:56:18 +0000

    도전 3

--- DAY3/DECRYPTION.py
+++ DAY3/DECRYPTION.py
@@ -1,7 +1,9 @@
+import time
+
 s = input('암호화된 문자열을 입력하세요 :')
 mo = ['a','e','i','o']
 lst =[]
-
+start_time = time.time()
 for i in range(len(s)):
     if (s[i-1] in mo and s[i] =='g') or (s[i] in mo and s[i+1] =='g') :
         #if s[i+1] =='g':
@@ -12,5 +14,6 @@ for i in range(len(s)):
         lst.append(s[i])
     
 print(''.join(lst))
-
+end_time = time.time()
+print("time:", end_time - start_time)
 
